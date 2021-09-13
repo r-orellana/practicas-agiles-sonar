4af2c9ce4a64dccc4183b3fed9e8f10688a7d621
Gonzalo Tixilima <g.tixilima@uniandes.edu.co>
2021-09-12 21:31:11 -0500

    [skip-ci] Configure sandbox (#15)
    * configure sandbox deploy
    
    * fix shipitfile format

--- shipitfile.js
+++ shipitfile.js
@@ -1,56 +1,63 @@
 module.exports = (shipit) => {
-    require("shipit-deploy")(shipit);
-  
-    shipit.initConfig({
-      default: {
-        deployTo: "/var/projects/ionic",
-        repositoryUrl: "git@github.com:MISW-4102-ProcesosDeDesarrolloAgil/MISW4201-202114-Grupo10.git",
-        keepReleases: 2,
-        shallowClone: true,
-        copy: false,
-      },
-      staging: {
-        servers: "ubuntu@staging.javoweb.io",
-        branch: "main",
-      }
+  require("shipit-deploy")(shipit);
+  const branch = require("git-branch");
+
+  shipit.initConfig({
+    default: {
+      deployTo: "/var/projects/ionic",
+      repositoryUrl:
+        "git@github.com:MISW-4102-ProcesosDeDesarrolloAgil/MISW4201-202114-Grupo10.git",
+      keepReleases: 2,
+      shallowClone: true,
+      copy: false,
+    },
+    staging: {
+      servers: "ubuntu@staging.javoweb.io",
+      branch: "main",
+    },
+    sandbox: {
+      servers: "ubuntu@sandbox.javoweb.io",
+      branch: branch.sync(),
+    },
+  });
+
+  shipit.on("updated", () => {
+    shipit.start("ionic:build");
+  });
+
+  shipit.on("published", () => {
+    shipit.start("ionic:restart");
+  });
+
+  shipit.blTask("ionic:build", async () => {
+    await shipit.start(["ionic:build:backend", "ionic:build:frontend"]);
+  });
+
+  shipit.blTask("ionic:build:frontend", async () => {
+    await shipit.remote("npm install production", {
+      cwd: `${shipit.releasePath}/frontend`,
     });
-  
-    shipit.on("updated", () => {
-      shipit.start("ionic:build");
+    await shipit.remote("NODE_ENV=production npm run build -- --localize", {
+      cwd: `${shipit.releasePath}/frontend`,
     });
-  
-    shipit.on("published", () => {
-      shipit.start("ionic:restart");
+  });
+
+  shipit.blTask("ionic:build:backend", async () => {
+    await shipit.remote("python3 -m venv .venv", {
+      cwd: shipit.releasePath,
     });
-  
-    shipit.blTask("ionic:build", async () => {
-      await shipit.start(["ionic:build:backend", "ionic:build:frontend"]);
+    await shipit.remote("source .venv/bin/activate && pip install wheel", {
+      cwd: shipit.releasePath,
     });
-  
-    shipit.blTask("ionic:build:frontend", async () => {
-      await shipit.remote("npm install production", {
-        cwd: `${shipit.releasePath}/frontend`
-      });
-      await shipit.remote("NODE_ENV=production npm run build -- --localize", {
-        cwd: `${shipit.releasePath}/frontend`
-      });
+    await shipit.remote(
+      "source .venv/bin/activate && pip install -r requirements.txt",
+      { cwd: shipit.releasePath }
+    );
+  });
+
+  shipit.blTask("ionic:restart", async () => {
+    await shipit.remote("sudo systemctl restart ionic-backend.service", {
+      tty: true,
     });
-  
-    shipit.blTask("ionic:build:backend", async () => {
-      await shipit.remote("python3 -m venv .venv", {
-        cwd: shipit.releasePath
-      });
-      await shipit.remote(
-        "source .venv/bin/activate && pip install wheel",
-        { cwd: shipit.releasePath }
-      );
-      await shipit.remote(
-        "source .venv/bin/activate && pip install -r requirements.txt",
-        { cwd: shipit.releasePath }
-      );
-    });
-  
-    shipit.blTask("ionic:restart", async () => {
-      await shipit.remote("sudo systemctl restart ionic-backend.service", { tty: true });
-    });
-  };
+  });
+};
